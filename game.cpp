cef499d448b7dbebcd3a851c4955f9fd8725368f
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2023-12-15 07:15:00 -0800

    Just added the basic layout of the code. I accidentally deleted all the code I had :(

---
+++ game.cpp
@@ -0,0 +1,17 @@
+//A simple game were you control the jerk, of a jerk, so that you can bully people.
+//Setup
+//more setup
+//Homescreen mainloop
+//	get events
+//		cursor (can be used to move to the start location)
+//		display the window
+//Game mainloop
+//	get events
+//		arrows(move)
+//		space(bully if your close enough to someone who has your item or, if their is nobody, but their is a bully, then you can knock them down before they bully you)
+//		a (add item to your bulliers inventory if your close enough to something)
+//		cursor (can be used to exit back to homescreen)
+//	display all characters
+//	move all characters (acording to a set speed as pixels/second)
+//	randomly spawn an item
+//	randomly spawn a character
