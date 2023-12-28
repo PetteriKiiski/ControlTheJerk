f7e130f139d4bc8bf0b931c8e557ea21a8820456
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2023-12-27 23:45:38 -0800

    Finally displayed an image

--- game.cpp
+++ game.cpp
@@ -1,12 +1,33 @@
 //A simple game were you control the jerk, of a jerk, so that you can bully people.
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include <iostream>
 #include <vector>
 using namespace std;
+//Bully Class
+class Jerk {
+private:
+	//setup variables
+	int acceleration = 0;
+	SDL_Surface * image = IMG_Load("Images/jerk.png");
+	SDL_Rect pos;
+public:
+	Jerk(SDL_Window* win) {
+		pos.x = 0;
+		pos.y = 0;
+		pos.w = 50;
+		pos.h = 100;
+		SDL_BlitSurface(image, NULL, SDL_GetWindowSurface(win), &pos);
+	};
+//function to move the bully
+//function to display the bully
+};
 //Homescreen mainloop
-const char* Home(SDL_Window* win) {
+const char* Home(SDL_Window * win) {
 	SDL_Event ev;
+	Jerk jerk(win);
 	while (true){
+		SDL_UpdateWindowSurface(win);
 		while (SDL_PollEvent(&ev)) {
 			if (ev.type == SDL_QUIT) {
 				return "Quit";
