fdab552c53bf6b6c0bffd99a8be9d5c43bf14b80
Petteri Kiiski <petteri.daniel.kiiski@gmail.com>
2024-01-04 13:21:09 -0800

    Trying to move my jerk

--- game.cpp
+++ game.cpp
@@ -3,7 +3,9 @@
 #include <SDL2/SDL_image.h>
 #include <iostream>
 #include <vector>
+#include <chrono>
 using namespace std;
+using namespace chrono;
 //to check if a point collides with a rect
 bool pointRectCollision (int x, int y, SDL_Rect rect) {
 	if (x >= rect.x and x <= rect.x + rect.w and y >= rect.y and y <= rect.y + rect.h) {
@@ -18,6 +20,10 @@ bool RectCollision (SDL_Rect rect1, SDL_Rect rect2) {
 	}	
 	return false;
 }
+
+uint64_t epochTime() {
+	return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
 SDL_Rect makeRect(int x, int y, int w, int h) {
 	SDL_Rect rect;
 	rect.x = x;
@@ -30,24 +36,27 @@ SDL_Rect makeRect(int x, int y, int w, int h) {
 class Jerk {
 	private:
 		//setup variables
-		int acceleration = 0;
+		int jerk[2] = {0, 0}; //Paired: [x-axis, y-axis]
+		int acceleration[2] = {0, 0};
+		int velocity[2] = {0, 0};
 		SDL_Surface * image = IMG_Load("Images/jerk.png");
 		SDL_Rect pos;
+		SDL_Window* win;
 	public:
-		Jerk() {
-			pos.x = 0;
-			pos.y = 0;
+		Jerk(SDL_Window* window) {
+			pos.x = 475;
+			pos.y = 280;
 			pos.w = 50;
 			pos.h = 100;
+			win = window;
 		}
-		void display(SDL_Window* win) {
+		void display() {
 			SDL_BlitSurface(image, NULL, SDL_GetWindowSurface(win), &pos);
 		}
-
-		SDL_Rect getPosition() {
-			return pos;
-		}
 		//function to move the bully
+		void move() {
+			
+		}
 		//function to display the bully
 };
 //Button class
@@ -55,12 +64,14 @@ class Button {
 	private:
 		SDL_Surface * image;
 		SDL_Rect pos;
+		SDL_Window* win;
 	public:		
-		Button(const char* file, SDL_Rect position) {
+		Button(const char* file, SDL_Rect position, SDL_Window* window) {
 			image = IMG_Load(file);
 			pos = position;
+			win = window;
 		}
-		void display(SDL_Window* win) {
+		void display() {
 			SDL_BlitSurface(image, NULL, SDL_GetWindowSurface(win), &pos);
 		}
 		bool clicked(int x, int y) {
@@ -72,12 +83,12 @@ const char* home(SDL_Window * win) {
 	//Setup images and rects
 	SDL_Rect home_page_pos = makeRect(0, 0, 1000, 660);
 	SDL_Event ev;
-	Button button("Images/start.png", makeRect(350, 510, 300, 75));
-	SDL_Surface * home_page = IMG_Load("Images/homepage.png");
+	Button button("Images/start.png", makeRect(350, 510, 300, 75), win);
+	SDL_Surface* home_page = IMG_Load("Images/homepage.png");
 	//No animation, no need to do this inside the mainloop
-	SDL_Surface * win_surface = SDL_GetWindowSurface(win);
+	SDL_Surface* win_surface = SDL_GetWindowSurface(win);
 	SDL_BlitSurface(home_page, NULL, win_surface, &home_page_pos);
-	button.display(win);
+	button.display();
 	SDL_UpdateWindowSurface(win);
 	while (true) {
 		//get events
@@ -90,16 +101,27 @@ const char* home(SDL_Window * win) {
 				if (button.clicked(ev.button.x, ev.button.y)) {
 					return "Game";
 				}
-				cout << ev.button.x << endl;
-				cout << ev.button.y << endl;
 			}
 		}
 	}
 }
 //Game mainloop
 const char* game(SDL_Window* win) {
-	return "NOT READY";
-
+	Jerk jerk(win);
+	SDL_Event ev;
+	SDL_Surface* bg = IMG_Load("Images/bg.png");
+	SDL_Rect bg_rect = makeRect(0, 0, 1000, 600);
+	while (true) {
+		SDL_BlitSurface(bg, NULL, SDL_GetWindowSurface(win), &bg_rect);
+		jerk.display();
+		SDL_UpdateWindowSurface(win);
+		while (SDL_PollEvent(&ev)) {
+			if (ev.type == SDL_QUIT) {
+				return "Quit";
+			}
+			//arrows(move)
+		}
+	}
 //	get events
 //		arrows(move)
 //		space(bully if your close enough to someone who has your item or, if their is nobody, but their is a bully, then you can knock them down before they bully you)
@@ -113,6 +135,7 @@ const char* game(SDL_Window* win) {
 }
 //main function(will call the mainloop functions)
 int main(int argc, char** args) {
+	cout << epochTime() << endl;
 	//Setup
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* win = SDL_CreateWindow("Control the Jerk", 0, 0, 1000, 660, 0);
